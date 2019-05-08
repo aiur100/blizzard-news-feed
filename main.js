b65ccae40e44b018f9ef642e71dc69cbfab5b466
cat_whisperer <cat_whisperer@cat-whisperers-MacBook-Pro.local>
2019-05-07 21:58:49 -0400

    Added empty functions for josh to finish

---
+++ main.js
@@ -0,0 +1,22 @@
+//TODO: We need to use npm to install fetch for node.
+//fetch is a "library" that we will use to call data from the
+//web. Google node-fetch. Look at the documents.
+
+function getHtmlFromWebPage(webUrl)
+{
+    //TODO: returns the HTML string from
+    //a given webUrl
+}
+
+function parseArticlOverviewDataHtml(webHtml)
+{
+  //IF article data is found, this should return
+  //a list of objects.  Each object should have a
+  // "title" and "url", field.
+  //We will likely use regex
+}
+
+function parseFullArticleDataHtml(webHtml)
+{
+  //Parse out the full article 
+}
